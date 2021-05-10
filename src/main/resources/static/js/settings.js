7876e9954f880d302281ebadbebc54f700f60f35
M. Claros <maxsiriusa0@gmail.com>
2021-05-10 20:08:08 +0200

    Added settings

---
+++ src/main/resources/static/js/settings.js
@@ -0,0 +1,35 @@
+function showUser() {
+
+
+    
+ };
+function showCountry() {
+
+
+    
+ };
+function showRegion() {
+
+
+    
+ };
+function showCity() {
+
+
+    
+ };
+function showSector() {
+
+
+    
+ };
+function showStatus() {
+
+
+    
+ };
+function showPaymentStatus() {
+
+
+    
+ };
