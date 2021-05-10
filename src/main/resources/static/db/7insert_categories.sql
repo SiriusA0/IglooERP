cf9ec6034086a2bdfd39278f20666980e97b45bd
M. Claros <maxsiriusa0@gmail.com>
2021-05-10 21:18:23 +0200

    Corrected categories.sql, added FullQuery.sql

--- src/main/resources/static/db/7insert_categories.sql
+++ src/main/resources/static/db/7insert_categories.sql
@@ -4,3 +4,4 @@ insert into categories (id, name) values (1, '1A');
 insert into categories (id, name) values (2, '1B');
 insert into categories (id, name) values (3, '2A');
 insert into categories (id, name) values (4, '2B');
+insert into categories (id, name) values (5, '3B');
