7c5cb6943a0bbbc5007d3fdcae901dfc5b909e3f
AriElisa8 <ariannaha91@gmail.com>
2021-05-11 17:01:02 +0200

    Controladores Cliente

--- src/main/java/com/igloo/client/controller/ClientController.java
+++ src/main/java/com/igloo/client/controller/ClientController.java
@@ -1,4 +1,41 @@
 package com.igloo.client.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.igloo.agent.model.Agent;
+import com.igloo.client.model.Client;
+import com.igloo.client.service.ClientRepository;
+import com.igloo.client.service.ClientService;
+
+
+
 public class ClientController {
+	
+
+	    @Autowired
+	    private ClientService clientServ;
+	    
+	    @GetMapping("/client")
+	    public String readAgent(Model model) {
+
+	        List<Client> clients = clientServ.get();
+
+	        model.addAttribute("clients", clients);
+
+
+	        return "client/clientlist";
+	    }
+	
+	
+	    
+	    
+	    
+	    
+	    
+	    
+	    
 }
--- src/main/java/com/igloo/client/service/ClientService.java
+++ src/main/java/com/igloo/client/service/ClientService.java
@@ -1,4 +1,25 @@
 package com.igloo.client.service;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.igloo.client.model.Client;
+
+
+@Service
 public class ClientService {
+    @Autowired
+    private ClientRepository clientRepo;
+
+    public List<Client> get() {
+
+        return  clientRepo.findAll();
+
+    }
+    
+    
+    
 }
+
+
+
